fb5e43c411da6df429bbad6de3e53a54f0ce0070
khl3ej <khl3ej@virginia.edu>
2018-07-26 13:37:55 -0400

    add backend logic for wikipedia article scraping; some front-end homepage design

--- app/views.py
+++ app/views.py
@@ -1,43 +1,61 @@
 # views.py
 
 # Kenny Le
+# Python application to randomly select and return a movie's title and plot using wikipediaapi
 
-import wikipediaapi
 import wikipedia
+import wikipediaapi
+import random
 import requests
 from bs4 import BeautifulSoup
-import random
-from flask import render_template
+from flask import render_template, request
 from app import app
 
-base_url = 'https://en.wikipedia.org'
-wiki_url = 'https://en.wikipedia.org/wiki/List_of_horror_films_of_2018'
+wiki_url = 'https://en.wikipedia.org/wiki/List_of_horror_films_of_2011'
 list_of_titles = []
 
 req = requests.get(wiki_url)
 soup = BeautifulSoup(req.content, 'lxml')
 table = soup.find('table', {'class':'wikitable sortable'})
 
-start_substring = 'title="'
-end_substring = '">'
-first_letter_of_title = ''
+#################### Helper Methods ####################
 
-for row in table.findAll('tr'):
-    cell = row.findAll('th')
-    str_cell = str(cell)
-    start_pt = str_cell.find(start_substring)
-    first_letter_of_title = str_cell[start_pt + 7: start_pt + 8]
-    end_substring = end_substring + first_letter_of_title
-    if start_substring in str_cell:
-        end_pt = str_cell.find(end_substring)
-        article = str_cell[start_pt + 7: end_pt]
-        list_of_titles.append(article)
+# return title and plot information
+def get_title_and_section_content(page):
+    try:
+        ret_text = page.sections[0].text
+        if len(ret_text) >= 250:
+            ret_text = ret_text.replace('\\','')
+        return page.title, ret_text
+    except IndexError:
+        return page.title, "No plot information available."
+    return page.title, "No plot information available."
+
+# randomly select title from list
+def get_random_movie(movies):
+    wiki = wikipediaapi.Wikipedia('en')
+    wiki_page = wiki.page(movies[random.randint(0, len(movies)-1)])
+    return wiki_page
+
+# parse for movie titles for use with wikipediaapi
+def fill_list_of_titles(wikitable, var_list):
+    start_substring = 'title="'
     end_substring = '">'
     first_letter_of_title = ''
+    for row in table.findAll('tr'):
+        cell = row.findAll('th')
+        str_cell = str(cell)
+        start_pt = str_cell.find(start_substring)
+        first_letter_of_title = str_cell[start_pt + 7: start_pt + 8]
+        end_substring = end_substring + first_letter_of_title
+        if start_substring in str_cell:
+            end_pt = str_cell.find(end_substring)
+            title = str_cell[start_pt + 7: end_pt]
+            var_list.append(title)
+        end_substring = '">'
+        first_letter_of_title = ''
 
-for title in list_of_titles:
-    print(title)
-
+# main
 @app.route('/')
 def index():
     return render_template('index.html')
